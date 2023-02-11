56451246bc3db5992099bdb80d18ab5e5b4a7cc8
minoshw <minosh97.lk@gmail.com>
2023-02-11 17:45:39 +0530

    new

--- inputworker.js
+++ inputworker.js
@@ -1,26 +1,22 @@
-function updateCountdown()
-{
-  var date =        document.getElementById('dateInput').value;
-              
-            var inpDate = new Date(date);
-            var currDate = new Date();
-              
-            if(inpDate.setHours(0, 0, 0, 0) == 
-                    currDate.setHours(0, 0, 0, 0))
-            {
-               document.getElementById("countdownText").innerText = "The input date is today's date. Please enter another date.";
-            }
-else
-{                let inputDate = new Date(document.getElementById("dateInput").value);
-                let currentDate = new Date();
-                let diff = inputDate.getTime() - currentDate.getTime();
-                let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-                let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                let countdownText = `${days} days and ${hours} hours until ${inputDate.toString()}`;
-                document.getElementById("countdownText").innerText = countdownText;
-}}
-function clearCountdown()
-    {
-document.getElementById("countdownText").innerText = "0 days and 0 hours";
-document.getElementById("dateInput").value = "";
+var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    $('#dateInput').attr('min',today);
+
+function updateCountdown() {
+      var dateInput = document.getElementById("dateInput").value;
+      var currentDate = new Date();
+      var inputDate = new Date(dateInput);
+
+      var timeDifference = inputDate.getTime() - currentDate.getTime();
+
+      if (timeDifference < 0) {
+        document.getElementById("countdownText").innerHTML = "Please enter a date in the future.";
+      } else {
+        var daysUntil = Math.ceil(timeDifference / (1000 * 3600 * 24));
+        document.getElementById("countdownText").innerHTML = daysUntil + " days until " + inputDate.toDateString();
+      }
     }
