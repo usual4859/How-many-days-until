94de104302d1a061d11e1da90570bf96533f2063
minoshw <minosh97.lk@gmail.com>
2023-02-11 12:35:52 +0530

    new css

--- inputworker.js
+++ inputworker.js
@@ -1,13 +1,9 @@
-      // Get the input field
-      var input = document.getElementById("dateInput");
-      // Execute a function when the user releases a key on the keyboard
-      input.addEventListener("change", function(event) {
-          // Calculate the number of days until the given date
-          var date1 = new Date();
-          var date2 = new Date(input.value);
-          var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-          var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-          
-          // Update the DOM with the number of days
-          document.getElementById("days").textContent = diffDays;
-      });
+function updateCountdown() {
+                let inputDate = new Date(document.getElementById("dateInput").value);
+                let currentDate = new Date();
+                let diff = inputDate.getTime() - currentDate.getTime();
+                let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+                let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                let countdownText = `${days} days and ${hours} hours until ${inputDate.toString()}`;
+                document.getElementById("countdownText").innerText = countdownText;
+            }
